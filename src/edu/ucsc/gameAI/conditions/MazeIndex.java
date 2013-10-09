c13755ccb59eff25c188c2a9f7375c553a73276e
EARink <edi.ramirez@gmail.com>
2013-10-09 14:17:19 -0700

    Added a couple classes
    Not much.

---
+++ src/edu/ucsc/gameAI/conditions/MazeIndex.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package edu.ucsc.gameAI.conditions;
+
+import edu.ucsc.gameAI.ICondition;
+
+/**
+ * @author Ed Ramirez
+ *
+ */
+public class MazeIndex implements ICondition {
+
+	int index;
+	
+	public MazeIndex(int _index)
+	{
+		
+	}
+	
+	@Override
+	public boolean test() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/edu/ucsc/gameAI/decisionTrees/binary/TreeAction.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package edu.ucsc.gameAI.decisionTrees.binary;
+
+import edu.ucsc.gameAI.IAction;
+
+/**
+ * @author Ed Ramirez
+ *
+ */
+public class TreeAction implements IBinaryNode {
+
+	/* (non-Javadoc)
+	 * @see edu.ucsc.gameAI.decisionTrees.binary.IBinaryNode#makeDecision()
+	 */
+	@Override
+	public IAction makeDecision() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
