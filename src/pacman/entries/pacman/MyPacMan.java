6ebe3c7a6fc8a122bd7e64e1ee51e1e5dbbb052d
Kevin <kevin@kevin-Gateway-Viper-C.(none)>
2013-10-14 15:15:46 -0700

    added a getMove() function to actions.

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 import edu.ucsc.gameAI.*;
 import edu.ucsc.gameAI.conditions.*;
-import edu.ucsc.gameAI.hfsm2.HState;
+import edu.ucsc.gameAI.hfsm2.HFSMFull;
 import edu.ucsc.gameAI.hfsm2.HTransition;
 import pacman.controllers.Controller;
 import pacman.game.Constants.MOVE;
@@ -18,21 +18,51 @@ import pacman.game.Game;
 public class MyPacMan extends Controller<MOVE>
 {
 	private MOVE myMove=MOVE.NEUTRAL;
-	private HState root;	
+	private HFSMFull root;	
 	public MyPacMan(){
 		super();
-		root = new HState();
-		root.setUpdateAction(new ConsolePrintAction("FSM Update"));
+		generateFSM();
+	}
+
+	protected void generateFSM(){
+		root = new HFSMFull("root");
+		
+		//Find the shortest path to the nearest pill
+		HFSMFull gather = new HFSMFull("gather", root);
+		gather.setEntryAction(new ConsolePrintAction("begin gather state"));
+		gather.setUpdateAction(new ConsolePrintAction("gather state", new GoRightAction()));
+				
+		//There is a ghost in Pac-Man's way to the next pill. Try to get him out of the way.
+		HFSMFull lure = new HFSMFull("lure", root);
+		lure.setEntryAction(new ConsolePrintAction("begin lure state"));
+		lure.setUpdateAction(new ConsolePrintAction("lure state", new GoLeftAction()));
+		
+		//There is a ghost following Pac-Man. Try to shake it off.
+		HFSMFull retreat = new HFSMFull("retreat", root);
+		retreat.setEntryAction(new ConsolePrintAction("begin retreat state"));
+		retreat.setUpdateAction(new ConsolePrintAction("retreat state"));
+		
+		//Pac-Man has just eaten a power pill. Eat all the ghosts!
+		HFSMFull rampage = new HFSMFull("rampage", root);
+		rampage.setEntryAction(new ConsolePrintAction("begin rampage state"));
+		rampage.setUpdateAction(new ConsolePrintAction("rampage state"));
+	}
+	
+	protected void generateTrees(){
 		
 	}
 	
 	public MOVE getMove(Game game, long timeDue) 
 	{
+		myMove = MOVE.NEUTRAL;
 		//Place your game logic here to play the game as Ms Pac-Man
 		ArrayList<IAction> actions = root.getActions();
 		if (!actions.isEmpty()){
 			for (IAction a: actions){
 				a.doAction();
+				if (a.getMove() != MOVE.NEUTRAL){
+					myMove = a.getMove();
+				}
 			}
 		}
 		return myMove;
