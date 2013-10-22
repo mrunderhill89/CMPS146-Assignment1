bda5e36c7c5cbd665a958342e0ef707c38d3578a
EARink <edi.ramirez@gmail.com>
2013-10-21 17:57:50 -0700

    Added some empty initialization code for ghost decisions

--- src/pacman/entries/ghosts/MyGhosts.java
+++ src/pacman/entries/ghosts/MyGhosts.java
@@ -1,6 +1,11 @@
 package pacman.entries.ghosts;
 
+import java.util.Dictionary;
 import java.util.EnumMap;
+
+import edu.ucsc.gameAI.*;
+import edu.ucsc.gameAI.conditions.*;
+import edu.ucsc.gameAI.decisionTrees.binary.BinaryDecision;
 import pacman.controllers.Controller;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
@@ -13,13 +18,56 @@ import pacman.game.Game;
 public class MyGhosts extends Controller<EnumMap<GHOST,MOVE>>
 {
 	private EnumMap<GHOST, MOVE> myMoves=new EnumMap<GHOST, MOVE>(GHOST.class);
+	private Dictionary<GHOST, BinaryDecision> ghostDecisionTrees;
+	
+	public MyGhosts() {
+		ghostDecisionTrees.put(GHOST.BLINKY, initBlinky());
+		ghostDecisionTrees.put(GHOST.PINKY, initPinky());
+		ghostDecisionTrees.put(GHOST.INKY, initInky());
+		ghostDecisionTrees.put(GHOST.SUE, initSue());
+	}
+	
+	private BinaryDecision initBlinky() {
+		BinaryDecision tree = new BinaryDecision();
+		
+		return tree;
+	}
+	
+	private BinaryDecision initPinky() {
+		BinaryDecision tree = new BinaryDecision();
+		
+		return tree;
+	}
+	
+	private BinaryDecision initInky() {
+		BinaryDecision tree = new BinaryDecision();
+		
+		return tree;
+	}
+	
+	private BinaryDecision initSue() {
+		BinaryDecision tree = new BinaryDecision();
+		
+		return tree;
+	}
 	
 	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue)
 	{
 		myMoves.clear();
 		
 		for (GHOST ghost : GHOST.values()) {
-			myMoves.put(ghost, MOVE.DOWN);
+//			BinaryDecision decision = new BinaryDecision();
+//			decision.setCondition(new IsEdible(game, ghost));
+//			decision.setTrueBranch(new GoLeftAction());
+//			decision.setFalseBranch(new GoUpAction());
+//			
+//			IAction action = decision.makeDecision(game);
+//			myMoves.put(ghost, action.getMove());
+			
+			BinaryDecision tree = ghostDecisionTrees.get(ghost); 
+			IAction action = tree.makeDecision(game);
+			myMoves.put(ghost, action.getMove());
+			
 		}
 		
 		return myMoves;
